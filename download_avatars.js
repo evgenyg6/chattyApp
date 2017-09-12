31a4197bbd5c58981790eeebc0c941e621218335
evgenyg6 <evgenyg@live.com>
2017-09-12 01:03:46 +0000

    Things are going well, really. Finished stretch task 2, it was swell.

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,25 @@
+var fs = require('fs');
 var request = require('request');
-var GITHUB_USER = "evgenyg6";
-var GITHUB_TOKEN = "fede263adbb99053d153072d57c33d756f4fffc6";
-console.log('Welcome to the GitHub Avatar Downloader!');
+require('./secretKey.env');
+var user = username;
+var token = pass;
+
+
+var repoOwner = process.argv[2]; //takes in user input
+var repoName = process.argv[3];
+
+function checkArguments(owner, name) {
+  if (owner === undefined || name === undefined) {
+    console.log("Please enter valid an owner and name!");
+  } else {
+    console.log('Welcome to the GitHub Avatar Downloader!');
+    getRepoContributors(repoOwner, repoName, getAvatar);
+  }
+}
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
-  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var requestURL = 'https://' + user + ':' + token + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   var string = '';
   console.log(requestURL);
   /////////////////////////////////////////////////////
@@ -18,27 +32,42 @@ function getRepoContributors(repoOwner, repoName, cb) {
   ////////////////////////////////////////////////////
   request.get(object) //on error
     .on('error', function(err) {
-      throw err;
+      console.log(err);
     })
 
   .on('data', function(data) { //on data recieving
     string += data;
-    console.log('Chunk Received: ', data.toString());
   })
 
   .on('end', function() { //on data recieve end
     console.log("<-----Response stream conplete.----->")
-    string = JSON.parse(string);
-    cb(string);
+    string = JSON.parse(string); //pare JSON object to string
+    cb(string); //callback for functions below to invoke on string
   });
 
 }
 
-function getAvatar(jaSON, imgPath) {
+function getAvatar(jaSON) { //loops through .avatar_url and logs
 
-  for (var i = 1; i < jaSON.length; i++) {
-    console.log(jaSON[i].avatar_url);
-  }
+  for (i in jaSON) {
+    downloadImageByURL(jaSON[i].avatar_url, 'avatars', jaSON[i].login);
+  } //iterates through everyone's avatar_url, places them in folder 'avatars', itirates through each login name and assigns it to file
 }
 
-console.log(getRepoContributors('jquery', 'jquery', getAvatar));
+function downloadImageByURL(url, filePath, userName) {
+
+  request.get(url)
+    .on('error', function(err) { //on error
+      console.log(err);
+    })
+
+  .on('data', function(data) { //on data recieving
+    console.log('Downloading images... ');
+  })
+
+  .on('end', function() { //on data recieve end
+    console.log("Download complete!");
+  }).pipe(fs.createWriteStream(filePath + "/" + userName));
+
+}
+checkArguments(repoOwner, repoName);
