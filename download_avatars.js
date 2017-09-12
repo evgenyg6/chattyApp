f512dade155d7bd1023e87de307aea4eafc65aa1
evgenyg6 <30935084+evgenyg6@users.noreply.github.com>
2017-09-11 21:23:19 -0600

    Update download_avatars.js

--- download_avatars.js
+++ download_avatars.js
@@ -4,10 +4,10 @@ require('./secretKey.env');
 var user = username;
 var token = pass;
 
-
+  
 var repoOwner = process.argv[2]; //takes in user input
 var repoName = process.argv[3];
-
+////////////////////////////////////////////////////////////
 function checkArguments(owner, name) {
   if (owner === undefined || name === undefined) {
     console.log("Please enter valid an owner and name!");
@@ -18,20 +18,20 @@ function checkArguments(owner, name) {
     getRepoContributors(repoOwner, repoName, getAvatar);
   }
 }
-
+////////////////////////////////////////////////////////////
 function getRepoContributors(repoOwner, repoName, cb) {
 
   var requestURL = 'https://' + user + ':' + token + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   var string = '';
   console.log(requestURL);
-  /////////////////////////////////////////////////////
+ 
   var object = { //adds user-agent object with your ID
     url: requestURL,
     headers: {
       "User-Agent": "evgenyg6"
     }
   };
-  ////////////////////////////////////////////////////
+
   request.get(object) //on error
     .on('error', function(err) {
       console.log(err);
@@ -48,30 +48,30 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 
 }
-
+////////////////////////////////////////////////////////////
 function getAvatar(jaSON) { //loops through .avatar_url and logs
 
   for (i in jaSON) {
     downloadImageByURL(jaSON[i].avatar_url, 'avatars', jaSON[i].login);
   } //iterates through everyone's avatar_url, places them in folder 'avatars', itirates through each login name and assigns it to file
 }
-
-function downloadImageByURL(url, filePath, username) { // This function downloads the files to a folder of choosing, names the file to user login, and adds an extension to each
-  var stream = request.get(url)
-    .on('error', function(err) {
+////////////////////////////////////////////////////////////
+function downloadImageByURL(url, filePath, username) { // This function downloads the files to a folder of choosing, names the file to user login
+  var stream = request.get(url) //passes url into function
+    .on('error', function(err) { 
       throw err;
     })
     .on('response', function(response) {
-      extension = response.headers["content-type"].split("/")
-      if (!fs.existsSync(filePath)) {
+      extension = response.headers["content-type"].split("/") //
+      if (!fs.existsSync(filePath)) { //if folder chosen doesn't exist, make one
         fs.mkdirSync(filePath);
       }
       console.log('Downloading image...');
-      stream.pipe(fs.createWriteStream(filePath + "/" + username + "." + extension[1]));
+      stream.pipe(fs.createWriteStream(filePath + "/" + username + "." + extension[1])); //downloads files and places in folder
     })
     .on('end', function(response) {
       console.log('Download complete.');
     })
-
 }
-checkArguments(repoOwner, repoName);
+////////////////////////////////////////////////////////////
+checkArguments(repoOwner, repoName); //call main function
